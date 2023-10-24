a9a3889c3e8367a008ceaae9cd38ef0ef5649536
Filip-Lukasik <147503758+Filip-Lukasik@users.noreply.github.com>
2023-10-24 13:36:26 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,26 +1,37 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+class ClientAccount:
+    def __init__(self,name_of_client):
+        self.client_name=name_of_client
+        self.stored_value=0    
+
+    def input_money(self,value):
+        self.stored_value+=value
+
+    def withdraw_money(self,value):
+        if value>self.stored_value:
+            print("There is not enough money in your account to withdraw {}$".format(value))
+            return
+        self.stored_value-=value
+
+    def print_amount_of_money_on_account(self):
+        print("there is {}$ on your account".format(self.stored_value))    
+
+
+class Bank:
+    def __init__(self,name_of_bank):
+        self.bank_name=name_of_bank
+        self.list_of_accounts=[]
+
+    def create_account(self,name_of_client):
+        self.list_of_accounts.append(ClientAccount(name_of_client))    
+
+def make_transation(name_of_bank_of_account_sending_money,name_of_client_sending_money,name_of_bank_of_account_getting_money,name_of_client_getting_money,value):
+    for account in name_of_bank_of_account_sending_money.list_of_accounts:
+        if account.client_name==name_of_bank_of_account_sending_money:
+            account.withdraw_money(value)
+    for account in name_of_bank_of_account_getting_money.list_of_accounts:
+        if account.client_name==name_of_bank_of_account_getting_money:
+            account.input_money(value)        
+
+if __name__ == "__main__":
+    BigBank=Bank("BigBank")
+    BigBank.create_account("John")
